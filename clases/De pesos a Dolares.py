addd2975d447598385b22c0ef915d734841b1791
Balthael <ing.johnosorio@gmail.com>
2022-12-05 12:55:54 -0500

    Este es mi primer programa

---
+++ clases/De pesos a Dolares.py
@@ -0,0 +1,4 @@
+pesos=float(input("Introduzca los Pesos que desea convertir a Dolares: "))
+VDolar=float(input("En cuanto esta el valor del dolar en pesos? "))
+CDolar=float(pesos/VDolar)
+print("El cambio correspondiente de {} de pesos en en  Dolares son: {}".format(pesos,CDolar))
